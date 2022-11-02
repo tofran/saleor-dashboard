834a9790310565fb6c5e8b381908fadd9ec2918c
Francisco Marques <franciscopcmarques@gmail.com>
2022-11-02 12:24:46 +0000

    API_URL docker replacement in runtime

---
+++ nginx/replace-api-url-docker-entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Replaces the original API_URL from the bundle's index.html files with the onethe $API_URL env var.
+# This avoids having to rebuild the bundle, making it possible to use a distributed Docker image.
+# This script is automatically picked up by the nginx entrypoint on startup.
+
+set -e
+
+INDEX_BUNDLE_PATH="/app/dashboard/index.html"
+
+if [[ -z "${API_URL}" ]]; then
+    echo "No API_URL provided, using defaults."
+else
+    echo "Setting API_URL to: $API_URL"
+
+    sed -i "s#SALEOR_API_URL = .*#SALEOR_API_URL = \"$API_URL\";#" "$INDEX_BUNDLE_PATH"
+fi
